c2c4fb26e4d55950c03e65279bc7ceb49cf657de
Damian <damian.peter.shaw@gmail.com>
2021-04-03 12:32:20 -0400

    Update tally_prime_gap.py

--- tally_prime_gap.py
+++ tally_prime_gap.py
@@ -11,15 +11,18 @@ some_primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
                401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
                467, 479, 487, 491, 499, 503, 509, 521, 523, 541}
 
+
 def miller_rabin_pass(a: int, s: int, d: int, n: int) -> bool:
     a_to_power = pow(a, d, n)
     if a_to_power == 1:
         return True
-    for i in range(s-1):
+
+    for _ in range(s-1):
         if a_to_power == n - 1:
             return True
         a_to_power = (a_to_power * a_to_power) % n
     return a_to_power == n - 1
+
 
 def primality_test(n: int) -> bool:
     '''
@@ -45,7 +48,7 @@ def primality_test(n: int) -> bool:
         tests = (2, 3, 5, 7, 11)
     else:
         raise ValueError("I can't be bothered writting a fully"
-                         "deterministic miller_rabin test")
+                         " deterministic miller_rabin test")
 
     d = n - 1
     s = 0
@@ -60,18 +63,32 @@ def primality_test(n: int) -> bool:
 
 
 def count_prime_gaps_per_chunk(chunk_start: int, chunk_end: int) -> Counter:
+    """
+    When we split our search in to chunks we have to make sure we're not
+    missing the prime gaps that occur over the boundry of said chunks.
+
+    We do that by searching for the previous prime before the chunk starts.
+    That will our first "previous prime".
+    """
+    # In general we are only search on odd numbers
     if chunk_start % 2 == 0:
-        chunk_start  += 1
-    # Find Previous Prime First:
-    for candidate in range(chunk_start, 1, -1):
+        chunk_start += 1
+    
+    # Special case for 3 as there are no odd primes smaller than 3
+    if chunk_start == 3:
+        chunk_start = 5
+
+    # Find Previous Prime First
+    for candidate in range(chunk_start - 2, 1, -1):
         if primality_test(candidate):
             prev_prime = candidate
             break
     else:
         raise ValueError("You did something wrong")
 
+    # Start counting the gaps!
     gap_counter = Counter()
-    for candidate in range(chunk_start + 2, chunk_end, 2):
+    for candidate in range(chunk_start, chunk_end, 2):
         if primality_test(candidate):
             gap_counter[candidate - prev_prime] += 1
             prev_prime = candidate
@@ -80,9 +97,21 @@ def count_prime_gaps_per_chunk(chunk_start: int, chunk_end: int) -> Counter:
 
 
 def main():
-    number_of_chunks = 100
+    # Number of chunks should be at least 2x the number of cores on your CPU
+    number_of_chunks = 101
+
+    # Maximum values should be MUCH larger than number of chunks
     maximum_value = 150_000_000
 
+    # When we spawn a worker to go and search prime gaps this gives
+    # us a "future" object, which is a sort of place holder.
+    #
+    # The "future" object will in the future give us the result by
+    # calling the "result()" method on it.
+    #
+    # How do we know when it's ready? The "with" statement handles
+    # that for us, it will wait until it completes, so after the "with"
+    # statement we know it's ready
     futures = []
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for i in range(number_of_chunks):
@@ -92,8 +121,10 @@ def main():
                 executor.submit(count_prime_gaps_per_chunk, start_of_chunk, end_of_chunk)
                 )
     
+    # The Counter objects we get back are very nicely summable
     gap_counter = sum((future.result() for future in futures), Counter())
     pprint(gap_counter.most_common())
+
 
 if __name__ == '__main__':
     main()
